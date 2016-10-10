d8f4b4c6c248c05dce266b1671c22a46efb79025
Paul Stiverson <thismatters@users.noreply.github.com>
2016-10-10 15:40:09 -0500

    Create main.c

---
+++ software/main.c
@@ -0,0 +1,10 @@
+// Set up timer
+
+// initialize
+
+// main run loop
+  // scan the input channels
+  // every so often change which output channel is active
+  
+  // do functions according to how the knobs are set
+  
